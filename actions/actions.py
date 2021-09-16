c6e407ca5500cb8bd132d256a251b2ea10df34de
sleevmichael <okofumichael@example.com>
2021-09-16 09:48:47 +0100

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,36 @@
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+
+class ActionInform(Action):
+
+    def name(self) -> Text:
+        return "action_inform"
+
+    def run(
+        self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        buttons=[
+            {""
+             "":'/customer_pin{"account":"agent"}', "title":"Agent"},
+            {"payload":'/agent_login{"account":"agent"}', "title": "User"},
+        ]
+        dispatcher.utter_message("Select the action you want to perform", buttons=buttons)
+        return []
+
+
+
+class ActionHelloWorld(Action):
+     def name(self) -> Text:
+         return "action_hello_world"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+         dispatcher.utter_message(text="Hello World!")
+         return []
